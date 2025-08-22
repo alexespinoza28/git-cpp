4aac50fe79acef220f14a04cbfbe032b018cf33e
Alex Espinoza <alexespinoza4528@gmail.com>
2025-08-22 14:53:25 -0700

    blob and main init

---
+++ Blob.hpp
@@ -0,0 +1,20 @@
+#ifndef BLOB_HPP
+#define BLOB_HPP
+
+#include <iostream>
+
+class Blob {
+
+    public: 
+
+    
+
+
+
+
+    private:
+
+
+};
+
+#endif // BLOB_HPP
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "Blob.hpp"
+
+int main() {
+    
+    return 0;
+}
